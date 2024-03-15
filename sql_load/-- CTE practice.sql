b4e76ea00e3097c7d0261649c028a6694ba6181a
AJ-Horch <josephhorch@gmail.com>
2024-03-15 15:58:11 +0000

    push example

--- sql_load/-- CTE practice.sql
+++ sql_load/-- CTE practice.sql
@@ -13,8 +13,19 @@ FROM job_postings_fact
 WHERE job_title_short = 'Data Analyst'
 GROUP BY location_category;
 -- problem 7, CTE example
-SELECT job_postings.job_id,
-    skill_id
-FROM skills_job_dim AS skills_to_job
-    INNER JOIN job_postings_fact AS job_postings ON job_postings.job_id = skills_to_job.job_id
-WHERE job_postings.job_work_from_home = TRUE
+WITH remote_job_skills AS (
+    SELECT skill_id,
+        COUNT(*) AS skill_count
+    FROM skills_job_dim AS skills_to_job
+        INNER JOIN job_postings_fact AS job_postings ON job_postings.job_id = skills_to_job.job_id
+    WHERE job_postings.job_work_from_home = TRUE
+        AND job_title_short = 'Data Scientist'
+    GROUP BY skill_id
+)
+SELECT skills.skill_id,
+    skills as skill_name,
+    skill_count
+FROM remote_job_skills
+    INNER JOIN skills_dim AS skills ON remote_job_skills.skill_id = skills.skill_id
+ORDER BY skill_count DESC
+LIMIT 5;
